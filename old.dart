5680b35972c04e4e608d7abee2a8f6f0a8d2f9b5
mkawsh77 <116841276+mkawsh77@users.noreply.github.com>
2023-02-07 17:27:53 +0300

    Add files via upload

---
+++ old.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+void main() {
+  //print("hellow world");
+//print("Enter the birth year");
+var old=1999;
+var age=DateTime.now().year-old;
+print("You the age is $age years");
+  
+}
+
